2fdca6a1124bbfb0ec5baf81e77970436368a215
amantur545 <amantur11@gmail.com>
2023-03-14 16:34:12 +0600

    tree

--- spam/views.py
+++ spam/views.py
@@ -1,11 +1,10 @@
 from django.shortcuts import render
-
-# Create your views here.
-from django.shortcuts import render
 from rest_framework import mixins, viewsets
 from spam.models import Contact
 from spam.serializers import ContactSerializer
 from rest_framework.permissions import IsAuthenticated
+
+
 
 class ContactAPIView(mixins.CreateModelMixin,
                     mixins.DestroyModelMixin,
